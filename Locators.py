65cecddb2571463b959353905539a0d3763ad175
sukjain <100119606+sukjain@users.noreply.github.com>
2022-02-25 12:57:32 +0530

    Add files via upload
    Mini_Assignment1

---
+++ Locators.py
@@ -0,0 +1,40 @@
+'''
+=>Goibibo Logo
+Goibibo_logo_By_xpath = //a[@class='gi-header-logo__home']
+
+
+=>Login or Signup-
+Login_Or_Signup_By_Xpath = //p[@class='gr-cap-text gr-blue-text gr-bold']
+Or
+Login_Or_Signup_By_Xpath = //div[@class='login__tab gotrible']
+
+
+=>Flights-
+Flight_By_xpath = //ul[@class='happy-nav']//li[1]
+Flight_By_link_text = 'Flights'
+
+
+=>Hotels-
+Hotels_By_xpath =  //ul[@class='happy-nav']//li[2]
+Hotels_By_link_text- 'Hotels'
+
+
+=>From-
+From_By_xpath=  //span[text()='From']
+
+
+=>Search-
+Search_By_xpath- //span[text()='SEARCH FLIGHTS']
+Search_By_class_name- //span[@class='sc-dFtzxp hwZghA']
+
+
+
+=>Student Fare-
+Student_Fare_By_xpath = //span[text()='student']
+
+
+
+=>Round Trips-
+Round_Trips_By_xpath- //span[text()='Round-trip']
+
+'''''
